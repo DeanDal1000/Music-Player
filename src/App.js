f4837bd138015b2815fd76c046cbcd0874699d9f
DeanDal1000 <dalzelldean@gmail.com>
2021-03-10 15:55:37 +0000

    Imported State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 import './styles/app.scss';
+import data from './util';
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
       <h1>Music</h1>
-      <Song />
+      {/* Pass State into curly braces */}
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
